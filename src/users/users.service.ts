cfe2d85d92884b5d3ecd4c05fb2784d924f2ba9b
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-03-28 12:08:29 -0500

    Add entity users and create and find user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async create(email: string, password: string) {
+    const user = this.userRepository.create({ email, password });
+    return this.userRepository.save(user);
+  }
+
+  async findByEmail(email: string) {
+    return this.userRepository.findOne({ where: { email } });
+  }
+
+  async findById(id: number) {
+    return this.userRepository.findOne({ where: { id } });
+  }
+}
