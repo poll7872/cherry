e59bb179da1579eee85305ee76c659fa8097d27d
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-03-30 14:26:12 -0500

    feat: update JWT config to use env variable and add EmailModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,14 +3,21 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { EmailModule } from 'src/email/email.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
-      secret: 'super-secret-jwt',
-      signOptions: { expiresIn: '2h' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '2h' },
+      }),
     }),
+    EmailModule,
   ],
   controllers: [AuthController],
   providers: [AuthService],
