900e08650b604ed98836a7ab4eefc18c586c2f3b
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-04-07 21:32:40 -0500

    refactor: update layout with new font and toast notification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Geist } from "next/font/google";
+import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { ToastNotification } from "@/components/ui/ToastNotification";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Cherry",
@@ -16,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
-      <body>{children}</body>
+    <html lang="es" className={cn("font-sans", inter.variable)}>
+      <body>
+        <ToastNotification />
+        {children}
+      </body>
     </html>
   );
 }
+
